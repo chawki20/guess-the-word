d60f81a10fc05897d435b139339713ee12ccfc11
Christina Hawkins <christinaehawkins@gmail.com>
2022-05-21 20:08:50 -0400

    Step2: Check input function, Validate input, capture input

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector (".message");
 const playAgainButton = document.querySelector (".play-again");
 
 const word = "magnolia";
+const allGuesses = [];
 
 //placeholders//
 const placeholder = function (word) { 
@@ -23,7 +24,37 @@ placeholder(word);
 //add event listener
 guessButton.addEventListener ("click", function (e){
     e.preventDefault();
+    message.innerText = "";
     const guess = letterInput.value;
-    console.log(guess);
-    letterInput.value = "";
+    const goodGuess = checkInput(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    }
+    letterInput.value ="";
 });
+
+//Checks the players input
+const checkInput = function (input){
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0){
+        message.innerText = "Please enter a letter A - Z!";
+    } else if (input.length > 1){
+        message.innerText = "Only 1 letter please.";
+    } else if (!input.match(acceptedLetter)){
+        message.innerText = "No numbers or special characters here, only letters please :)";
+    } else {
+        return input;
+    }
+};    
+
+//Captures the players input    
+const makeGuess = function (guess){
+    guess = guess.toUpperCase ();
+    if (allGuesses.includes (guess)) {
+        message.innerText = "This one again? Try another letter please :)";
+    } else {
+        allGuesses.push(guess);
+        console.log(allGuesses);
+    }
+};
